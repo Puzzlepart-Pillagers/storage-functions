ec7c1d5c9c996aaacae9a6f001be1b41417c93ed
Kim Damsleth <kim@damsleth.com>
2020-02-08 15:06:35 +0100

    refactoring

--- WageWar/index.ts
+++ WageWar/index.ts
@@ -21,8 +21,76 @@ const httpTrigger: AzureFunction = function (context: Context, req: HttpRequest)
         }
         context.done();
     } else {
-        wageWar(attacking, defending).then((result) => {
-            notifyWarRoom(result[0], result[1])
+        wageWar(attacking, defending, context)
+    }
+};
+
+// THESE ARE MAIL ADDRESSES
+function wageWar(attacking, defending, context) {
+    Promise.all([
+        got(`${functionURL}/GetKing?email=${attacking}`).json(),
+        got(`${functionURL}/GetKing?email=${defending}`).json(),
+        got(`${functionURL}/GetUnits?email=${attacking}`).json(),
+        got(`${functionURL}/GetUnits?email=${defending}`).json()]).then(res => {
+            let attackerRes = res[0]
+            let defenderRes = res[1]
+            let atUnits = res[2]
+            let defUnits = res[3]
+            let attacker = attackerRes.value[0];
+            let defender = defenderRes.value[0];
+            let atFullName = `${attacker.FirstName} ${attacker.LastName}`
+            let defFullName = `${defender.FirstName} ${defender.LastName}`
+            let atPenning = attacker.Penning
+            let defPenning = defender.Penning
+            // number of units for attacker and defender
+            let atUnitCount = atUnits.value.length;
+            let defUnitCount = defUnits.value.length;
+            // total xp == all units XP summed
+            let atXP = 0;
+            let defXP = 0;
+            atUnits.value.forEach(unit => atXP += unit.XP)
+            defUnits.value.forEach(unit => defXP += unit.XP)
+            let atBuf = parseFloat(attacker.Buff);
+            let defBuf = parseFloat(defender.Buff);
+            //total xp of all units + 1% pr buff
+            let atBuffedXP = atXP * (1 + atBuf / 100)
+            let defBuffedXP = atXP * (1 + defBuf / 100)
+            // max 50% buff
+            if (atBuffedXP > (atXP * 1.5)) { atBuffedXP = atXP * 1.5 }
+            if (defBuffedXP > (defXP * 1.5)) { defBuffedXP = defXP * 1.5 }
+            let sumStrength = atBuffedXP + defBuffedXP;
+            let atChance = (atBuffedXP * 100) / sumStrength;
+            let defChance = (defBuffedXP * 100) / sumStrength;
+            // sum of randomisation + strength
+            let atSum = Math.round(atChance * Math.random())
+            let defSum = Math.round(defChance * Math.random())
+
+            // total sum
+            let warSum = atSum + defSum;
+
+            // sum of losses for attacker and defender
+            let atLossPct = (defSum / warSum);
+            let defLossPct = (atSum / warSum);
+
+            let atUnitLoss = Math.round(atUnitCount * atLossPct);
+            let defUnitLoss = Math.round(defUnitCount * defLossPct);
+
+            let atWon = defLossPct > atLossPct;
+            let atPenningLossGain = Math.round(atWon ? defPenning * defLossPct : -(atPenning * atLossPct));
+            let defPenningLossGain = Math.round(atWon ? -(defPenning * defLossPct) : atPenning * atLossPct);
+
+            let title = `WAR REPORT - ${atFullName} v. ${defFullName}`.toString();
+            let txt = `**${atWon
+                ? attacker.FirstName
+                : defender.FirstName} WINS**   \nUnits lost:
+               \n${attacker.FirstName}: **${atUnitLoss.toString()}**
+               \n${defender.FirstName}: **${defUnitLoss.toString()}**
+               \nPenning forfeited:
+                \n${atWon
+                    ? `**${attacker.FirstName}** steals **₰${atPenningLossGain.toString()}** from **${defender.FirstName}**`
+                    : `**${defender.FirstName}** steals **₰${defPenningLossGain.toString()}** from **${attacker.FirstName}**`}`;
+            // return [title, txt];
+            notifyWarRoom(title, txt);
             console.log("WAR OVER, details in chat")
             context.res = {
                 status: 200,
@@ -30,73 +98,6 @@ const httpTrigger: AzureFunction = function (context: Context, req: HttpRequest)
             }
             context.done();
         })
-    }
-};
-
-// THESE ARE MAIL ADDRESSES
-async function wageWar(attacking, defending): Promise<Array<any>> {
-    try {
-        let attackerRes = await got(`${functionURL}/GetKing?email=${attacking}`).json();
-        let defenderRes = await got(`${functionURL}/GetKing?email=${defending}`).json();
-        let atUnits = await got(`${functionURL}/GetUnits?email=${attacking}`).json();
-        let defUnits = await got(`${functionURL}/GetUnits?email=${defending}`).json();
-        let attacker = attackerRes.value[0];
-        let defender = defenderRes.value[0];
-        let atFullName = `${attacker.FirstName} ${attacker.LastName}`
-        let defFullName = `${defender.FirstName} ${defender.LastName}`
-        let atPenning = attacker.Penning
-        let defPenning = defender.Penning
-        // number of units for attacker and defender
-        let atUnitCount = atUnits.value.length;
-        let defUnitCount = defUnits.value.length;
-        // total xp == all units XP summed
-        let atXP = 0;
-        let defXP = 0;
-        atUnits.value.forEach(unit => atXP += unit.XP)
-        defUnits.value.forEach(unit => defXP += unit.XP)
-        let atBuf = parseFloat(attacker.Buff);
-        let defBuf = parseFloat(defender.Buff);
-        //total xp of all units + 1% pr buff
-        let atBuffedXP = atXP * (1 + atBuf / 100)
-        let defBuffedXP = atXP * (1 + defBuf / 100)
-        // max 50% buff
-        if (atBuffedXP > (atXP * 1.5)) { atBuffedXP = atXP * 1.5 }
-        if (defBuffedXP > (defXP * 1.5)) { defBuffedXP = defXP * 1.5 }
-        let sumStrength = atBuffedXP + defBuffedXP;
-        let atChance = (atBuffedXP * 100) / sumStrength;
-        let defChance = (defBuffedXP * 100) / sumStrength;
-        // sum of randomisation + strength
-        let atSum = Math.round(atChance * Math.random())
-        let defSum = Math.round(defChance * Math.random())
-
-        // total sum
-        let warSum = atSum + defSum;
-
-        // sum of losses for attacker and defender
-        let atLossPct = (defSum / warSum);
-        let defLossPct = (atSum / warSum);
-
-        let atUnitLoss = Math.round(atUnitCount * atLossPct);
-        let defUnitLoss = Math.round(defUnitCount * defLossPct);
-
-        let atWon = defLossPct > atLossPct;
-        let atPenningLossGain = Math.round(atWon ? defPenning * defLossPct : -(atPenning * atLossPct));
-        let defPenningLossGain = Math.round(atWon ? -(defPenning * defLossPct) : atPenning * atLossPct);
-
-        let title = `WAR REPORT - ${atFullName} v. ${defFullName}`.toString();
-        let txt = `**${atWon
-            ? attacker.FirstName
-            : defender.FirstName} WINS**   \nUnits lost:
-               \n${attacker.FirstName}: **${atUnitLoss.toString()}**
-               \n${defender.FirstName}: **${defUnitLoss.toString()}**
-               \nPenning forfeited:
-                \n${atWon
-                ? `**${attacker.FirstName}** steals **₰${atPenningLossGain.toString()}** from **${defender.FirstName}**`
-                : `**${defender.FirstName}** steals **₰${defPenningLossGain.toString()}** from **${attacker.FirstName}**`}`;
-        return [title, txt];
-    } catch (err) {
-        console.error(err)
-    }
 }
 
 export default httpTrigger;
